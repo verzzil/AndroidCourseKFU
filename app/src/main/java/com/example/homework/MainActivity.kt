69bef29264bb599718902cccfafbf7bc43e9e843
Verzzil <xannanov.albert@mail.ru>
2020-10-15 22:31:57 +0300

    homework 4 (complete app)

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -3,37 +3,61 @@ package com.example.homework
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var lastPhoto: ImageView? = null
+    var selectedIc: ImageView? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var lastPhoto : ImageView? = null
-        var selectedIc : ImageView? = null
-
         first.setOnClickListener {
-            selectedIc?.isSelected = false
-            it.isSelected = true
-            selectedIc = it as ImageView
-
-            lastPhoto?.visibility = View.INVISIBLE
-            photo1.visibility = View.VISIBLE
-            lastPhoto = photo1
-
+            changeSelected(it)
+            changePhoto(photo1)
+            photo1.startAnimation(AnimationUtils.loadAnimation(this, R.anim.scale))
         }
 
         second.setOnClickListener {
-            selectedIc?.isSelected = false
-            it.isSelected = true
-            selectedIc = it as ImageView
+            changeSelected(it)
+            changePhoto(photo2)
+            photo2.startAnimation(AnimationUtils.loadAnimation(this, R.anim.scale))
+        }
 
-            lastPhoto?.visibility = View.INVISIBLE
-            photo2.visibility = View.VISIBLE
-            lastPhoto = photo2
+        third.setOnClickListener {
+            changeSelected(it)
+            changePhoto(photo3)
+            photo3.startAnimation(AnimationUtils.loadAnimation(this, R.anim.scale))
+        }
 
+        fourth.setOnClickListener {
+            changeSelected(it)
+            changePhoto(photo4)
+            photo4.startAnimation(AnimationUtils.loadAnimation(this, R.anim.scale))
+        }
+
+        fifth.setOnClickListener {
+            changeSelected(it)
+            changePhoto(photo5)
+            photo5.startAnimation(AnimationUtils.loadAnimation(this, R.anim.scale))
         }
     }
+
+    private fun changeSelected(currentElement: View) {
+        selectedIc?.isSelected = false
+        currentElement.isSelected = true
+        selectedIc = currentElement as ImageView
+    }
+
+    private fun changePhoto(currentPhoto: ImageView) {
+        lastPhoto?.visibility = View.INVISIBLE
+        currentPhoto.visibility = View.VISIBLE
+        lastPhoto = currentPhoto
+    }
+
 }
